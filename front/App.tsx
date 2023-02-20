41dea8f38babe7eb6ead854d3bd02341f1eef0d9
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-02-20 16:48:16 +0100

    dev: added showSplashScreen state

--- front/App.tsx
+++ front/App.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {SplashScreen} from './screens/SplashScreen';
 import {displayName} from './app.json';
 import {version} from './package.json';
@@ -6,8 +6,15 @@ import {LoginScreen} from './screens/LoginScreen';
 import {MenuScreen} from './screens/MenuScreen';
 
 const App = () => {
-  const showSplashScreen = false;
+  const [showSplashScreen, setShowSplashScreen] = useState(true);
   const [isConnected, setIsConnected] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log('set to false');
+      setShowSplashScreen(false);
+    }, 500);
+  }, []);
 
   const onConnected = useCallback(() => {
     setIsConnected(true);
