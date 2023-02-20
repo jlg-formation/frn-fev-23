b67217ecda55f12da141d8989947209975679b17
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-02-20 16:08:03 +0100

    dev: added login screen

--- front/screens/LoginScreen.tsx
+++ front/screens/LoginScreen.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 
 export const LoginScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Login Screen</Text>
+      <Text style={styles.h1}>Connexion</Text>
+      <View style={styles.form}>
+        <View style={styles.label}>
+          <Text>Login</Text>
+          <TextInput />
+        </View>
+        <View style={styles.label}>
+          <Text>Mot de passe</Text>
+          <TextInput />
+        </View>
+        <Button title="Se connecter" />
+      </View>
     </View>
   );
 };
@@ -16,4 +27,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     height: '100%',
   },
+  h1: {},
+  form: {},
+  label: {},
 });
