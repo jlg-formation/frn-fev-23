c10471d6e2a11f3ebcf17294ea7b7b6be792ccd1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-02-20 17:24:14 +0100

    dev: added legal screen

--- front/screens/MenuScreen.tsx
+++ front/screens/MenuScreen.tsx
@@ -2,6 +2,7 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import {HomeScreen} from './menu/HomeScreen';
+import {LegalScreen} from './menu/LegalScreen';
 import {SettingScreen} from './menu/SettingScreen';
 
 const Tab = createBottomTabNavigator();
@@ -11,6 +12,7 @@ export const MenuScreen = () => {
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Legal" component={LegalScreen} />
         <Tab.Screen name="Settings" component={SettingScreen} />
       </Tab.Navigator>
     </NavigationContainer>
