28c25fbbd8ac359fe75d8a8237baa643ed5054cc
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-02-21 11:52:34 +0100

    dev: added state isDisconnecting

--- front/screens/menu/SettingScreen.tsx
+++ front/screens/menu/SettingScreen.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
-import {Pressable, StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  ActivityIndicator,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import {Locale} from '../../interfaces/Locale';
 import {useAuthenticationStore} from '../../stores/authentication.store';
 import {primaryColor} from '../../theme';
 
 export function SettingScreen() {
+  const [isDisconnecting, setIsDisconnecting] = useState(false);
   const authenticationStore = useAuthenticationStore();
 
   const changeLanguage = (newLocale: Locale) => () => {
     console.log('newLocale: ', newLocale);
   };
 
-  const disconnect = () => {
+  const disconnect = async () => {
     console.log('disconnect');
-    authenticationStore.disconnect();
+    setIsDisconnecting(true);
+    await authenticationStore.disconnect();
+    setIsDisconnecting(false);
   };
   return (
     <View style={styles.container}>
@@ -21,6 +30,7 @@ export function SettingScreen() {
       <View style={styles.section}>
         <Text style={styles.h2}>Langues</Text>
         <Pressable
+          android_ripple={{color: 'gray'}}
           style={[styles.button, styles.primaryButton]}
           onPress={changeLanguage('fr')}>
           <Text style={[styles.buttonText, styles.primaryButtonText]}>
@@ -28,6 +38,7 @@ export function SettingScreen() {
           </Text>
         </Pressable>
         <Pressable
+          android_ripple={{color: 'gray'}}
           style={[styles.button, styles.primaryButton]}
           onPress={changeLanguage('en')}>
           <Text style={[styles.buttonText, styles.primaryButtonText]}>
@@ -38,11 +49,16 @@ export function SettingScreen() {
       <View style={styles.section}>
         <Text style={styles.h2}>Compte</Text>
         <Pressable
+          android_ripple={{color: 'gray'}}
           style={[styles.button, styles.secondaryButton]}
           onPress={disconnect}>
-          <Text style={[styles.buttonText, styles.secondaryButtonText]}>
-            Se déconnecter
-          </Text>
+          {isDisconnecting ? (
+            <ActivityIndicator />
+          ) : (
+            <Text style={[styles.buttonText, styles.secondaryButtonText]}>
+              Se déconnecter
+            </Text>
+          )}
         </Pressable>
       </View>
     </View>
