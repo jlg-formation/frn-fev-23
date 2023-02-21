f0da139ab8af31f7153fc004e2fb07e105bafaa0
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-02-21 10:11:31 +0100

    dev: added settings DOM

--- front/screens/menu/SettingScreen.tsx
+++ front/screens/menu/SettingScreen.tsx
@@ -1,14 +1,39 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
+import {Locale} from '../../interfaces/Locale';
 
 export function SettingScreen() {
+  const changeLanguage = (newLocale: Locale) => () => {
+    console.log('newLocale: ', newLocale);
+  };
+
+  const disconnect = () => {
+    console.log('disconnect');
+  };
   return (
     <View style={styles.container}>
-      <Text>Settings!</Text>
+      <Text style={styles.h1}>Paramètres</Text>
+      <View>
+        <Text>Langues</Text>
+        <Pressable style={styles.button} onPress={changeLanguage('fr')}>
+          <Text>Français</Text>
+        </Pressable>
+        <Pressable style={styles.button} onPress={changeLanguage('en')}>
+          <Text>English</Text>
+        </Pressable>
+      </View>
+      <View>
+        <Text>Compte</Text>
+        <Pressable style={styles.button} onPress={disconnect}>
+          <Text>Se déconnecter</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {flex: 1, justifyContent: 'center', alignItems: 'center'},
+  h1: {},
+  button: {},
 });
