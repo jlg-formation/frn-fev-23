5bb602edbb7aa0b265377b044fcb8aff3bad5884
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-02-21 11:41:40 +0100

    dev: added sleep function on authentication store

---
+++ front/misc.ts
@@ -0,0 +1,7 @@
+export const sleep = (delay: number) => {
+  return new Promise<void>(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, delay);
+  });
+};
--- front/stores/authentication.store.ts
+++ front/stores/authentication.store.ts
@@ -1,22 +1,23 @@
 import {create} from 'zustand';
 import {User} from '../interfaces/User';
+import {sleep} from '../misc';
 
 export interface AuthenticationStore {
   user: User | undefined;
-  connect: () => void;
-  disconnect: () => void;
+  connect: () => Promise<void>;
+  disconnect: () => Promise<void>;
 }
 
 export const useAuthenticationStore = create<AuthenticationStore>(set => ({
   user: undefined,
-  connect: () => {
+  connect: async () => {
     console.log('connecting...');
-
+    await sleep(2000);
     set(() => ({user: {displayName: 'Jean-Louis GUENEGO'}}));
   },
-  disconnect: () => {
+  disconnect: async () => {
     console.log('disconnecting...');
-
+    await sleep(2000);
     set(() => ({user: undefined}));
   },
 }));
