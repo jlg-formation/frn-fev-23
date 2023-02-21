d01b79f77252675b089bc686aee0db8ce673a71e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-02-21 09:36:53 +0100

    dev: added icons on navigation

--- front/screens/MenuScreen.tsx
+++ front/screens/MenuScreen.tsx
@@ -5,12 +5,35 @@ import {HomeScreen} from './menu/HomeScreen';
 import {LegalScreen} from './menu/LegalScreen';
 import {SettingScreen} from './menu/SettingScreen';
 
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 const Tab = createBottomTabNavigator();
 
 export const MenuScreen = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          // eslint-disable-next-line react/no-unstable-nested-components
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName = '';
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-list' : 'ios-list-outline';
+            } else if (route.name === 'Legal') {
+              iconName = focused ? 'ios-list' : 'ios-list-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}>
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Legal" component={LegalScreen} />
         <Tab.Screen name="Settings" component={SettingScreen} />
