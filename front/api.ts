f6986c91e0c13499486a20d8a39890a17c97a6a1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-02-21 15:56:37 +0100

    dev: bad login error

--- front/api.ts
+++ front/api.ts
@@ -1,12 +1,32 @@
 import {User} from './interfaces/User';
-import {sleep} from './misc';
-
+import {domainUrl} from './app.json';
 class API {
   async connect(login: string, password: string): Promise<User> {
-    console.log('login: ', login);
-    console.log('password: ', password);
-    await sleep(2000);
-    throw new Error('Method not implemented.');
+    try {
+      console.log('login: ', login);
+      console.log('password: ', password);
+      const response = await fetch(domainUrl + '/api/auth/connect', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({email: login, password: password}),
+      });
+      if (response.status === 401) {
+        throw new Error('Bad Login');
+      }
+      if (response.status !== 200) {
+        throw new Error('Technical Error');
+      }
+      const user: User = await response.json();
+      return user;
+    } catch (err) {
+      console.log('err: ', err);
+      if (err instanceof Error && err.message === 'Bad Login') {
+        throw err;
+      }
+      throw new Error('Technical Error');
+    }
   }
 }
 
