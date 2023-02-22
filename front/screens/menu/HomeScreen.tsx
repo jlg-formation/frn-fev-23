6e65be9ec3212b442631f682c3c7e884d9c15479
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-02-22 09:33:41 +0100

    dev: added home page
    with 2 new components : article add and article list

--- front/screens/menu/HomeScreen.tsx
+++ front/screens/menu/HomeScreen.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, View} from 'react-native';
+import {ArticleAdd} from '../../widgets/ArticleAdd';
+import {ArticleList} from '../../widgets/ArticleList';
 
 export function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text>Home!</Text>
+      <Image style={styles.image} source={require('../../assets/jardin.jpg')} />
+      <ArticleAdd />
+      <ArticleList />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {flex: 1, justifyContent: 'center', alignItems: 'center'},
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  image: {
+    height: 200,
+    width: '100%',
+    objectFit: 'cover',
+  },
 });
