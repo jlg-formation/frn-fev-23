13051dc7d1cd2744ab6abb87a93780f3837195b8
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-02-22 16:49:47 +0100

    dev: added default language

--- front/stores/i18n.store.ts
+++ front/stores/i18n.store.ts
@@ -3,6 +3,22 @@ import {Locale} from '../interfaces/Locale';
 import enJson from '../i18n/en.json';
 import frJson from '../i18n/fr.json';
 
+import {Platform, NativeModules} from 'react-native';
+
+const deviceLanguage: string =
+  Platform.OS === 'ios'
+    ? NativeModules.SettingsManager.settings.AppleLocale ||
+      NativeModules.SettingsManager.settings.AppleLanguages[0] //iOS 13
+    : NativeModules.I18nManager.localeIdentifier;
+
+console.log(deviceLanguage); //en_US
+
+const defaultLocale = deviceLanguage.substring(0, 2) as Locale;
+
+const getJson = (locale: Locale) => {
+  return locale === 'fr' ? frJson : enJson;
+};
+
 export interface I18nStore {
   t: typeof enJson;
   locale: Locale;
@@ -10,10 +26,10 @@ export interface I18nStore {
 }
 
 export const useI18nStore = create<I18nStore>(set => ({
-  t: enJson,
-  locale: 'en',
+  t: getJson(defaultLocale),
+  locale: defaultLocale,
   changeLocale: newLocale => {
-    const t = newLocale === 'fr' ? frJson : enJson;
+    const t = getJson(newLocale);
     set({locale: newLocale, t: t});
   },
 }));
