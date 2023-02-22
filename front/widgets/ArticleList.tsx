e96c4d88d3d9013542a13471965f57f298a0fc30
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-02-22 14:07:53 +0100

    dev: articles can be viewed

--- front/widgets/ArticleList.tsx
+++ front/widgets/ArticleList.tsx
@@ -1,21 +1,10 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
-import {Article} from '../interfaces/Article';
+import {useArticleStore} from '../stores/article.store';
 import {ArticleItem} from './ArticleItem';
 
 export const ArticleList = () => {
-  const articles: Article[] = [
-    {
-      id: 'a1',
-      text: 'coucou',
-      imageUrls: [
-        'http://localhost:3000/images/user-background.jpg',
-        'http://localhost:3000/images/user-background.jpg',
-      ],
-    },
-    {id: 'a2', text: 'hello', imageUrls: []},
-    {id: 'a3', text: 'toto', imageUrls: []},
-  ];
+  const {articles} = useArticleStore();
   return (
     <View style={styles.container}>
       {articles.map(article => (
